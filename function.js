981c7aa27e017f0f5443367f8ae2f218b32c2333
mdayubali <ayubh9288@gmail.com>
2023-05-27 09:32:47 +0600

    add comment

--- function.js
+++ function.js
@@ -2,3 +2,15 @@
 // a variable can hold the value but a function can hold one or more complete line of code.
 
 // Function hoisting only works with function declarations — not with function expressions.
+
+function name(){
+    console.log('My name is Ayub Ali  ');
+    // there is no return value for this function
+}
+
+// calling/running/invoking function
+name();
+name();
+name();
+// A function have must a return value.
+// without without return value a funtion result will be undifined.

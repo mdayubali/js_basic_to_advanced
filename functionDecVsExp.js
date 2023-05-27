84255dd4281c6a6a8db9e98a409b047808e0b2be
mdayubali <ayubh9288@gmail.com>
2023-05-27 11:24:40 +0600

    added expression and function declaration

--- functionDecVsExp.js
+++ functionDecVsExp.js
@@ -1,6 +1,21 @@
 // function declaration vs Expression
+// function call with declaration and expression has the same result but you cann't use Hoisting for the expression.
 
+// Histing means call the function before declare 
+// Hoisting allow to fucntion declaration.
+const myName = yourName("Ali Ershad");
+console.log(myName)
+// Declare function
 function yourName(name){
     return name;
 }
-console.log(yourName('Md Ayub Ali'))
+// const myName = yourName("Ali Ershad");
+// console.log(myName)
+
+// Function expression
+// It's not allow for the hoisting
+// console.log(myName1('Md Ayub Ali')) 
+const myName1 = function(name){
+    return name;
+}
+console.log(myName1('Md Ayub Ali'))
